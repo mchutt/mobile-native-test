2f795a9e697a7c6fe7213c03d0af43a2ae64aa86
Mateo Chutt <mchutt@solvd.com>
2025-01-10 17:27:44 -0300

    Add verify drawing feature test

--- src/main/java/com/solvd/carina/swaglabs/pages/common/SideNavMenuBasePage.java
+++ src/main/java/com/solvd/carina/swaglabs/pages/common/SideNavMenuBasePage.java
@@ -1,5 +1,8 @@
 package com.solvd.carina.swaglabs.pages.common;
 
+import com.solvd.carina.swaglabs.pages.common.DrawingPageBase;
+import com.solvd.carina.swaglabs.pages.common.LoginPageBase;
+import com.zebrunner.carina.utils.factory.ICustomTypePageFactory;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractPage;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
@@ -10,13 +13,20 @@ public class SideNavMenuBasePage extends AbstractPage {
     @ExtendedFindBy(accessibilityId = "test-LOGOUT")
     private ExtendedWebElement logoutBtn;
 
+    @ExtendedFindBy(accessibilityId = "test-DRAWING")
+    private ExtendedWebElement drawingBtn;
+
     public SideNavMenuBasePage(WebDriver driver) {
         super(driver);
     }
 
-    public LoginPageBase clickOnLogoutBtn(){
+    public LoginPageBase clickOnLogoutBtn() {
         logoutBtn.click();
         return initPage(driver, LoginPageBase.class);
     }
 
+    public DrawingPageBase clickOnDrawingBtn() {
+        drawingBtn.click();
+        return initPage(driver, DrawingPageBase.class);
+    }
 }
