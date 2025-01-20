a4c9df5d8da3ee77bf1a0569cb1b166d7296f31e
Mateo Chutt <mchutt@solvd.com>
2025-01-20 20:51:58 -0300

    add data provider to LoginTest, verifyDrawingFeatureTest is working for android platform

--- src/main/java/com/solvd/carina/swaglabs/pages/android/DrawingPage.java
+++ src/main/java/com/solvd/carina/swaglabs/pages/android/DrawingPage.java
@@ -1,18 +1,17 @@
-package com.solvd.carina.swaglabs.pages.ios;
+package com.solvd.carina.swaglabs.pages.android;
 
 import com.solvd.carina.swaglabs.pages.common.DrawingPageBase;
+import com.zebrunner.carina.utils.android.IAndroidUtils;
 import com.zebrunner.carina.utils.factory.DeviceType;
-import com.zebrunner.carina.utils.ios.IOSUtils;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.decorator.PageOpeningStrategy;
 import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.WebDriver;
 
-@DeviceType(pageType = DeviceType.Type.IOS_PHONE, parentClass = DrawingPageBase.class)
-public class DrawingPage extends DrawingPageBase implements IOSUtils {
+@DeviceType(pageType = DeviceType.Type.ANDROID_PHONE, parentClass = DrawingPageBase.class)
+public class DrawingPage extends DrawingPageBase implements IAndroidUtils {
 
-
-    @ExtendedFindBy(accessibilityId = "Signature Pad demo")
+    @ExtendedFindBy(androidUIAutomator = "new UiSelector().className(\"android.webkit.WebView\").instance(0)")
     private ExtendedWebElement screen;
 
     @ExtendedFindBy(accessibilityId = "test-CLEAR")
--- src/main/java/com/solvd/carina/swaglabs/pages/common/DrawingPageBase.java
+++ src/main/java/com/solvd/carina/swaglabs/pages/common/DrawingPageBase.java
@@ -1,14 +1,37 @@
 package com.solvd.carina.swaglabs.pages.common;
 
+import com.zebrunner.carina.utils.mobile.IMobileUtils;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractPage;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 
-public abstract class DrawingPageBase extends AbstractPage {
+public abstract class DrawingPageBase extends AbstractPage implements IMobileUtils {
 
     public DrawingPageBase(WebDriver driver) {
         super(driver);
     }
 
     public abstract void drawShape();
+
     public abstract void clickOnCleanBtn();
+
+    protected void drawShape(ExtendedWebElement screen) {
+        Point position = screen.getLocation();
+        Dimension size = screen.getSize();
+
+        int startX = position.x + size.width / 4;
+        int startY = position.y + size.height / 6;
+        int endX = startX + size.width / 2;
+        int endY = startY - size.height / 10;
+
+        swipe(startX, startY, endX, startY, 1000);
+        swipe(endX, startY, endX, endY, 1000);
+        swipe(endX, endY, startX, endY, 1000);
+        swipe(startX, endY, startX, startY, 1000);
+        int midY = (startY + endY) / 2;
+        swipe(startX, midY, endX, midY, 1000);
+    }
+
 }
